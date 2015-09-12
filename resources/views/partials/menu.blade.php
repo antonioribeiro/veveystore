7b519f4cb9f5804e029354c38aea780ea7231f5e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-09-12 19:46:11 -0300

    Improve view organization

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+
+    <div class="span12">
+
+        <ul class="top-nav">
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Registre-se</a></li>
+            <li><a href="/password/recover">Perdeu a senha?</a></li>
+        </ul>
+
+    </div>
+
+</div>
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('templates.686.layout')
-
-@section('contents')
-    Those are our contents.
-@stop
